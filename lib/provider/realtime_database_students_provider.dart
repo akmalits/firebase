413b194b30df4864def93004c1f09e7852dce256
achmad.ajidkan <achmad.ajidkan@wowrack.com>
2024-06-12 12:36:57 +0700

    final speedometer

--- lib/provider/realtime_database_students_provider.dart
+++ lib/provider/realtime_database_students_provider.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fireeeee/model/student_model.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class RealTimeDatabaseProvider extends ChangeNotifier {
   RealTimeDatabaseProvider() {
@@ -35,4 +36,93 @@ class RealTimeDatabaseProvider extends ChangeNotifier {
       notifyListeners();
     });
   }
+
+  //==========================GRAPH============================
+
+  TrackballBehavior? _trackballBehaviors;
+
+  TrackballBehavior? get trackballBehaviors => _trackballBehaviors;
+
+  List<Color> color = [
+    const Color(0xFF08A3AA),
+    const Color(0xFF1766C5),
+    const Color(0xFFEF3F3F),
+    const Color(0xFFC12F87),
+    const Color(0xFF16777B),
+    const Color(0xFF0F3A6D),
+    const Color(0xFFAD2626),
+    const Color(0xFF882B47),
+    const Color(0xFF0FCAA8),
+    const Color(0xFF73B3FF),
+    const Color(0xFFFF6B17),
+    const Color(0xFFBB6BD9),
+    const Color(0xFF78CA0F),
+    const Color(0xFF00C7F2),
+    const Color(0xFFF2994A),
+    const Color(0xFF695CFB),
+    const Color(0xFF467A05),
+    const Color(0xFF7392FF),
+    const Color(0xFF984B04),
+    const Color(0xFF491C02),
+    const Color(0xFF08A3AA),
+    const Color(0xFF1766C5),
+    const Color(0xFFEF3F3F),
+    const Color(0xFFC12F87),
+    const Color(0xFF16777B),
+    const Color(0xFF0F3A6D),
+    const Color(0xFFAD2626),
+    const Color(0xFF882B47),
+    const Color(0xFF0FCAA8),
+    const Color(0xFF73B3FF),
+    const Color(0xFFFF6B17),
+    const Color(0xFFBB6BD9),
+    const Color(0xFF78CA0F),
+    const Color(0xFF00C7F2),
+    const Color(0xFFF2994A),
+    const Color(0xFF695CFB),
+    const Color(0xFF467A05),
+    const Color(0xFF7392FF),
+    const Color(0xFF984B04),
+    const Color(0xFF491C02),
+    const Color(0xFF08A3AA),
+    const Color(0xFF1766C5),
+    const Color(0xFFEF3F3F),
+    const Color(0xFFC12F87),
+    const Color(0xFF16777B),
+    const Color(0xFF0F3A6D),
+    const Color(0xFFAD2626),
+    const Color(0xFF882B47),
+    const Color(0xFF0FCAA8),
+    const Color(0xFF73B3FF),
+    const Color(0xFFFF6B17),
+    const Color(0xFFBB6BD9),
+    const Color(0xFF78CA0F),
+    const Color(0xFF00C7F2),
+    const Color(0xFFF2994A),
+    const Color(0xFF695CFB),
+    const Color(0xFF467A05),
+    const Color(0xFF7392FF),
+    const Color(0xFF984B04),
+    const Color(0xFF491C02),
+    const Color(0xFF08A3AA),
+    const Color(0xFF1766C5),
+    const Color(0xFFEF3F3F),
+    const Color(0xFFC12F87),
+    const Color(0xFF16777B),
+    const Color(0xFF0F3A6D),
+    const Color(0xFFAD2626),
+    const Color(0xFF882B47),
+    const Color(0xFF0FCAA8),
+    const Color(0xFF73B3FF),
+    const Color(0xFFFF6B17),
+    const Color(0xFFBB6BD9),
+    const Color(0xFF78CA0F),
+    const Color(0xFF00C7F2),
+    const Color(0xFFF2994A),
+    const Color(0xFF695CFB),
+    const Color(0xFF467A05),
+    const Color(0xFF7392FF),
+    const Color(0xFF984B04),
+    const Color(0xFF491C02),
+  ];
 }
--- lib/screen/realtime_database_screen.dart
+++ lib/screen/realtime_database_screen.dart
@@ -2,6 +2,8 @@ import 'package:fireeeee/provider/realtime_database_students_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fireeeee/model/student_model.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class RealTimeDatabase extends StatelessWidget {
   static const routeName = '/realtime_database_stless';
@@ -20,11 +22,140 @@ class RealTimeDatabase extends StatelessWidget {
             ),
             body: SingleChildScrollView(
               child: Column(
-                children: List<Widget>.generate(
-                  provider.studentList!.length,
-                  (index) =>
-                      studentWidget(context, provider.studentList![index]),
-                ),
+                children: [
+                  // SfCartesianChart(
+                  //     primaryXAxis: CategoryAxis(title: AxisTitle(text: 'Time')),
+                  //     // Enable legend
+                  //     palette: provider.color,
+                  //     legend: Legend(
+                  //       isVisible: true,
+                  //       position: LegendPosition.bottom,
+                  //       overflowMode: LegendItemOverflowMode.wrap,
+                  //     ),
+                  //     // Enable tooltip
+                  //     tooltipBehavior: TooltipBehavior(enable: true, header: '', canShowMarker: false),
+                  //     plotAreaBorderWidth: 0,
+                  //     primaryYAxis: NumericAxis(
+                  //       edgeLabelPlacement: EdgeLabelPlacement.shift,
+                  //       majorGridLines: const MajorGridLines(width: 0),
+                  //       // title: AxisTitle(text: 'Value', textStyle: AppTextStyles.baseTextStyle(fontWeight: FontWeight.w500)),
+                  //     ),
+                  //     trackballBehavior: provider.trackballBehaviors,
+                  //     zoomPanBehavior: ZoomPanBehavior(enablePinching: true, enablePanning: true),
+                  //     series: List.generate(provider.studentList!.length, (index) {
+                  //       return LineSeries<Student, String>(
+                  //           dataSource: provider.studentList,
+                  //           xValueMapper: (Student data, _) => data.studentData!.age,
+                  //           yValueMapper: (Student data, _) => num.parse(data.studentData!.age!),
+                  //           name: provider.studentList![index].studentData!.name!,
+                  //           enableTooltip: true,
+                  //           markerSettings: const MarkerSettings(isVisible: true, width: 2),
+                  //           animationDuration: 2500,
+                  //           width: 5,
+                  //           legendIconType: LegendIconType.circle,
+                  //           // Enable data label
+                  //           dataLabelSettings: const DataLabelSettings(isVisible: false));
+                  //     })
+                  //
+                  //   // series: <ChartSeries<Datas, String>>
+                  //   // [
+                  //   //   LineSeries<Datas, String>(
+                  //   //       dataSource: provider.listData,
+                  //   //       color: fourthColor,
+                  //   //       xValueMapper: (Datas data, _) {
+                  //   //         if (provider.minutes != null || provider.hour != null) {
+                  //   //           var date = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(data.times!, true);
+                  //   //           var n = DateFormat("HH:mm:ss").format(date.toLocal());
+                  //   //           return n;
+                  //   //         } else {
+                  //   //           var date = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(data.times!, true);
+                  //   //           var n = DateFormat("MM-dd HH:mm:ss").format(date.toLocal());
+                  //   //           return n;
+                  //   //         }
+                  //   //       },
+                  //   //       yValueMapper: (Datas data, _) => data.val,
+                  //   //       name: 'Telemetry',
+                  //   //       enableTooltip: true,
+                  //   //       animationDuration: 2500,
+                  //   //       width: 1,
+                  //   //       // Enable data label
+                  //   //       dataLabelSettings: DataLabelSettings(isVisible: false)),
+                  //   //   LineSeries<Datas, String>(
+                  //   //       dataSource: provider.allData![2].datas!,
+                  //   //       color: Colors.red,
+                  //   //       xValueMapper: (Datas data, _) {
+                  //   //         if (provider.minutes != null || provider.hour != null) {
+                  //   //           var date = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(data.times!, true);
+                  //   //           var n = DateFormat("HH:mm:ss").format(date.toLocal());
+                  //   //           return n;
+                  //   //         } else {
+                  //   //           var date = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(data.times!, true);
+                  //   //           var n = DateFormat("MM-dd HH:mm:ss").format(date.toLocal());
+                  //   //           return n;
+                  //   //         }
+                  //   //       },
+                  //   //       yValueMapper: (Datas data, _) => data.val,
+                  //   //       name: 'Telemetry',
+                  //   //       enableTooltip: true,
+                  //   //       animationDuration: 2500,
+                  //   //       width: 1,
+                  //   //       // Enable data label
+                  //   //       dataLabelSettings: DataLabelSettings(isVisible: false))
+                  //   // ],
+                  // ),
+                  SfRadialGauge(
+                    title: GaugeTitle(
+                        text: provider.studentList!.last.studentData!.age!,
+                        textStyle:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    enableLoadingAnimation: true,
+                    animationDuration: 4500,
+                    axes: <RadialAxis>[
+                      RadialAxis(minimum: 0, maximum: 14, pointers: <GaugePointer>[
+                        NeedlePointer(value: double.parse(provider.studentList!.last.studentData!.age!), enableAnimation: true)
+                      ], ranges: <GaugeRange>[
+                        GaugeRange(startValue: 0, endValue: 5, color: Colors.green),
+                        GaugeRange(startValue: 5, endValue: 10, color: Colors.orange),
+                        GaugeRange(startValue: 10, endValue: 14, color: Colors.red)
+                      ], annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                            widget: Text(
+                              '${provider.studentList!.last.studentData!.age!} MPH',
+                              style:
+                              TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                            ),
+                            positionFactor: 0.5,
+                            angle: 90)
+                      ])
+                    ],
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    reverse: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(provider.studentList!.last.studentData!.name.toString(), style: TextStyle(fontSize: 20),),
+                            SizedBox(height: 8,),
+                            Text('${provider.studentList!.last.studentData!.age.toString()} MPH', style: TextStyle(fontSize: 18)),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: 1,
+                  ),
+                ],
               ),
             ),
           );
